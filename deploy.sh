57c8358aa2b8e08af4b6ed0c721fa5dfbdf89021
Son Cao <sonthanh.cao@hotmail.com>
2024-09-22 23:06:32 +0700

    Update

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,7 @@ if [[ "$CI_COMMIT_TAG" != "" ]]; then
 fi
 # Replace variables in the YAML file
 sed -e "s/\$SERVICE_NAME/$SERVICE_NAME/g" -e "s/\$TAG/$TAG/g" ./deployment/main.yaml > ./deployment/k8s-main.yaml
-
+kubectl apply -f ./deployment/k8s-main.yaml
 if [[ $? != 0 ]]; then exit 1; fi
 
 kubectl rollout status deployments/$SERVICE_NAME
@@ -20,8 +20,3 @@ kubectl rollout status deployments/$SERVICE_NAME
 if [[ $? != 0 ]]; then
     kubectl logs $(kubectl get pods --sort-by=.metadata.creationTimestamp | grep "$SERVICE_NAME" | awk '{print $1}' | tac | head -1 ) --tail=20 && exit 1;
 fi
-
-
-
-
-kubectl apply -f ./deployment/main-k8s.yaml
