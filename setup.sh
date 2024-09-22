fa436232c1c6a0bfdf9496c7f5d8738388dfad03
Son Cao <sonthanh.cao@hotmail.com>
2024-09-22 23:25:19 +0700

    Update

--- setup.sh
+++ setup.sh
@@ -100,5 +100,6 @@ helm upgrade --install "$INSTALLATION_NAME" \
     --namespace "$NAMESPACE" \
     --create-namespace \
     --set githubConfigUrl="$GITHUB_CONFIG_URL" \
+    --set controllerServiceAccount.name=arc-gha-rs-controller \
     --set githubConfigSecret.github_token="$GITHUB_TOKEN" \
     oci://ghcr.io/actions/actions-runner-controller-charts/gha-runner-scale-set
